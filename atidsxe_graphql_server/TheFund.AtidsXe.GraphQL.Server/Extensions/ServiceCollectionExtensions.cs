ed6839052457c415a2d1264d07c15967c5ea7e24
Donell McCoy <donell.mccoy@cloudnhance.com>
2020-09-18 17:53:30 -0400

    Save point

---
+++ atidsxe_graphql_server/TheFund.AtidsXe.GraphQL.Server/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheFund.AtidsXe.GraphQL.Server.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+
+    }
+}
