d528b1dad75bd0e395f0c187c035175f6e8b8acb
Donell McCoy <donell.mccoy@cloudnhance.com>
2020-09-21 16:55:23 -0400

    Save point

---
+++ atidsxe_graphql_server/TheFund.AtidsXe.GraphQL.Server/Filters/BranchLocationFilterType.cs
@@ -0,0 +1,15 @@
+﻿using HotChocolate.Types.Filters;
+using TheFund.AtidsXe.Data.Entities;
+
+namespace TheFund.AtidsXe.GraphQL.Server.Filters
+{
+    public class BranchLocationFilterType : FilterInputType<BranchLocation>
+    {
+        protected override void Configure(IFilterInputTypeDescriptor<BranchLocation> descriptor)
+        {
+            descriptor.BindFieldsExplicitly()
+                .Filter(t => t.Description)
+                .AllowNotIn();
+        }
+    }
+}
