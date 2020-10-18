54a1c6b805730d623ec0607f4afc844639486e97
Donell McCoy <contact@cloudnhance.com>
2020-10-18 12:31:00 -0400

    Save point

---
+++ atidsxe_graphql_server/TheFund.AtidsXe.Console/TitleSearchOrigination.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TheFund.AtidsXe.Console
+{
+    public partial class TitleSearchOrigination
+    {
+        public int TitleSearchOriginationId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public string OrderReference { get; set; }
+
+        public int TitleEventId { get; set; }
+
+        public int FileReferenceId { get; set; }
+    }
+
+}
